19c08598ca94e5bab93eb2dbdc5acaebfb65537c
kasper <sebb@sebb.dk>
2017-10-01 22:47:47 +0200

    Updates

---
+++ _next/34dff980-c7dd-4cee-b177-51776146b5b8/page/tag/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/tag', function() {
+            var comp = module.exports=webpackJsonp([4],{570:function(e,t,u){e.exports=u(571)},571:function(e,t,u){"use strict";function l(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=u(7),n=l(a),r=u(129),d=l(r),f=u(128),o=l(f),c=u(142),s=l(c);u(85);var i=u(141),g=l(i);t.default=(0,g.default)(function(e){var t=e.url.query.tag;return n.default.createElement("div",null,n.default.createElement(d.default,{href:"/"},n.default.createElement("a",null,"Home")),n.default.createElement("h1",null,t),s.default.filter(function(e){return e.tags&&e.tags.includes(t)}).map(function(e){return n.default.createElement(o.default,{key:e.slug,title:e.title,slug:e.slug,date:e.date,author:e.author,body:e.body,tags:e.tags})}))},"Tag")}},[570]);
+            return { page: comp.default }
+          })
+        
